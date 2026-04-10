d502be7fe2d013b2fcf13c8630e96543bada2fd1
Musrh <musplay2023@gmail.com>
2026-04-10 02:55:02 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from './views/Home.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
     component: Home
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), // ⭐ IMPORTANT
   routes
 })
 
