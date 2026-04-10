80ba10b214ba7f020eb9e9f1ec7938096296d834
Musrh <musplay2023@gmail.com>
2026-04-10 02:09:47 +0100

    Create Reader.vue

---
+++ src/components/Reader.vue
@@ -0,0 +1,13 @@
+<script setup>
+const speak = (text) => {
+  const utterance = new SpeechSynthesisUtterance(text)
+  utterance.lang = 'fr-FR'
+  speechSynthesis.speak(utterance)
+}
+</script>
+
+<template>
+  <button @click="speak(text)">
+    🔊 Lire
+  </button>
+</template>
